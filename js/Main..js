3730b138b0b44a71772994863479c43b3f47f861
Jothinathan36 <109500742+Jothinathan36@users.noreply.github.com>
2023-08-27 16:50:48 +0530

    test navigation

--- js/Main..js
+++ js/Main..js
@@ -1,8 +1,10 @@
 var NavItems = document.getElementById("NavItems")
 
 function ShowMenu(){
+  NavItems.style.display = 'block';
   NavItems.style.right = '0';
 }
 function HideMenu(){
+  NavItems.style.display = 'none';
   NavItems.style.right = '-250px';
 }
